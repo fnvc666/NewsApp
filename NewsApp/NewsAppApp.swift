0af62577d0ee94bf90156fb08ac6edc24092677f
frainvoc <frainvoc@gmail.com>
2025-05-20 15:44:51 +0200

    init

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Павел Сивак on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
